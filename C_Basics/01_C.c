3a5cff8ad50669fa6c8cdd821d6c096e625423f1
rajnish-portal <info.rajnish2005@gmail.com>
2025-08-04 16:25:52 +0530

    hey

---
+++ C_Basics/01_C.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("hey world");
+    return 0;
+}
---
+++ C_Basics/03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int noa;
+    int nob;
+    printf("Write any natural no. :");
+    scanf("%d", &noa);
+    printf("Write any other natural no. :");
+    scanf("%d", &nob);
+
+    
+    printf("Your first Value was %d \n", noa);
+    printf("Your second Value was %d", nob);
+    return 0;
+
+
+}
